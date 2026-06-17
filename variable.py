3c5891e81b096eff6314f7240dcb16f265473559
filatova <FilatovMS@bcsfintech.ru>
2026-06-17 21:07:38 +0300

    Изучение переменных и типов данных

---
+++ variable.py
@@ -0,0 +1,20 @@
+number = 5 #int
+digit = 0.5 #float
+word = 'one' #string
+boolean1 = True #bool (1)
+boolean2 = False #bool (0)
+#
+# del number # Удалить переменную
+
+# print(number + digit)
+# # print(word + number) - не отработает
+# # print(word + boolean1) - не отработает
+# print(boolean1 + boolean2)
+# print(word * number)
+#
+# print(f"Result {number}") # Использование функциональной (f) строки. Тривиально: print("Result", number)
+#
+# print(word + str(digit + float(number))) # Приведение значения данных  нужному типу: str(), float(), int(), bool()
+
+input("")
+input("")
