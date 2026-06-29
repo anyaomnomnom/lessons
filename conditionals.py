cb5d44603bf259ab18db391e1bf7c59745b686c2
filatova <FilatovMS@bcsfintech.ru>
2026-06-29 21:01:18 +0300

    Условные операторы

---
+++ conditionals.py
@@ -0,0 +1,29 @@
+# a = int(input())
+# b = int(input())
+# if a == b: # != => =< > <
+#     print("yes")
+# else:
+#     print("no")
+
+# age = int(input("Введите возраст:"))
+# isHappy = False
+# if isHappy:
+#     print("I am happy")
+#     if age >= 18:
+#         print("Win")
+#     else:
+#         print("Sad")
+# elif not isHappy:
+#     print("I am sad")
+
+age = int(input("Введите возраст: "))
+if age >= 18:
+    print("ura")
+    if age == 33:
+        print("ne ura")
+elif age == 17:
+    print("1")
+if age == 16:
+    print("2")
+else:
+    print("3")
