1b30274247135909f1d347a7e2c24eacf53dbb24
filatova <FilatovMS@bcsfintech.ru>
2026-06-29 20:12:31 +0300

    Изучение переменных и типов данных

--- variable.py
+++ variable.py
@@ -14,8 +14,24 @@ boolean2 = False #bool (0)
 #
 # print(f"Result {number}") # Использование функциональной (f) строки. Тривиально: print("Result", number)
 #
-print(word, str(digit + float(number)), sep=' ') # Приведение значения данных  нужному типу: str(), float(), int(), bool()
-word = '1' #string
-print(word + str(digit + float(number)))
-# input("")
-# input("")
+#print(word, str(digit + float(number)), sep=' ') # Приведение значения данных  нужному типу: str(), float(), int(), bool()
+# word = '1' #string
+# print(word + str(digit + float(number)))
+
+# num1 = int(input("Введите число:"))
+# num2 = int(input("Введите число:"))
+#
+# result = num1 + 5
+# num2 += 9
+# num1 -= num2
+# print(result)
+# print(num2)
+# print(num1)
+#
+# word = "kaka"
+# print(word, (word * 3), sep=' ')
+# print(f"result {word * 5}")
+
+a = 4
+print(a / True) # True = 1, False = 0
+print(a / False)
